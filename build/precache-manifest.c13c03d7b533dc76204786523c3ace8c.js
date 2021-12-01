7098845038ad39f019d07c51791a8c9cef3094c4
Chukwunweike Obiora Igboanusi <cwertlinks@gmail.com>
2021-12-01 08:47:39 +0100

    chore: build files

---
+++ build/precache-manifest.c13c03d7b533dc76204786523c3ace8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "70005471ad073e756957",
+    "url": "/static/css/main.7eb5d3ab.chunk.css"
+  },
+  {
+    "revision": "70005471ad073e756957",
+    "url": "/static/js/main.70005471.chunk.js"
+  },
+  {
+    "revision": "eaf0cd25c29c73f7cc09",
+    "url": "/static/js/1.eaf0cd25.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "255670bb138342c37535b9f84ba7477d",
+    "url": "/index.html"
+  }
+];
